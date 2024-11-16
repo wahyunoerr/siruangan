0c3d566524fa18749df40c35ad4bfadd0222f03e
wahyunoerr <wahyunoer1001@gmail.com>
2024-11-16 22:36:15 +0700

    ganti warna purple

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -17,11 +17,11 @@
 
     <div class="wrapper">
         <!-- Sidebar -->
-        <div class="sidebar" data-background-color="dark">
+        <div class="sidebar" data-background-color="white">
             <div class="sidebar-logo">
 
                 <!-- Logo Header -->
-                <div class="logo-header" data-background-color="dark">
+                <div class="logo-header" data-background-color="purple2">
                     <a href="index.html" class="logo">
                         <img src="{{ asset('assets/dashboard/img/kaiadmin/logo_light.svg') }}" alt="navbar brand"
                             class="navbar-brand" height="20" />
--- resources/views/layouts/dashboard/navbar.blade.php
+++ resources/views/layouts/dashboard/navbar.blade.php
@@ -1,4 +1,5 @@
-.<nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
+<nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom"
+    data-background-color="purple2">
     <div class="container-fluid">
         <nav class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
             <div class="input-group">
